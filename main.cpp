a953ba3cc23e9d8872b86ed5cad2818fe014f796
Mack Hashemi <mack@hashemi.com>
2015-11-01 20:01:20 -0800

    Finalize main

--- main.cpp
+++ main.cpp
@@ -1,86 +1,23 @@
 #include <iostream>
 
+// uncomment line below to get debugging information
+//#define _DEBUG
+
 #include "Stack.h"
+#include "Calculator.h"
 
 using namespace std;
 
 int main(void)
 {
 
-  int i;
+  Calculator c;
   string s;
 
-  // test stack with numbers
-  cout << "Testing stack with integers" << endl
-       << "---------------------------" << endl;;
-  Stack<int> stack;
+  cout << "Enter an infix expression to be computed: ";
+  getline(cin, s);
 
-  cout << "Stack empty? " << ((stack.isEmpty() == true) ? "Yes" : "No") << endl;
-
-  cout << "Push 1" << endl;
-  stack.push(1);
-  cout << "Push 2" << endl;
-  stack.push(2);
-  cout << "Push 3" << endl;
-  stack.push(3);
-
-  cout << "Stack empty? " << ((stack.isEmpty() == true) ? "Yes" : "No") << endl;
-
-  i = stack.peek();
-  cout << "peek top of stack: " << i << endl;
-
-  cout << "pop top of stack" << endl;
-  stack.pop();
-
-  i = stack.peek();
-  cout << "peek top of stack: " << i << endl;
-
-  cout << "pop top of stack" << endl;
-  stack.pop();
-
-  i = stack.peek();
-  cout << "peek top of stack: " << i << endl;
-
-  cout << "pop top of stack" << endl;
-  stack.pop();
-
-  cout << "Stack empty? " << ((stack.isEmpty() == true) ? "Yes" : "No") << endl << endl;;
-
-  // test stack with strings
-  cout << "Testing stack with strings" << endl
-       << "--------------------------" << endl;;
-  Stack<string> strstack;
-
-  cout << "Stack empty? " << ((strstack.isEmpty() == true) ? "Yes" : "No") << endl;
-
-  cout << "Push \"California\"" << endl;
-  strstack.push("California");
-  cout << "Push \"New York\"" << endl;
-  strstack.push("New York");
-  cout << "Push \"Illinois\"" << endl;
-  strstack.push("Illinois");
-
-  cout << "Stack empty? " << ((strstack.isEmpty() == true) ? "Yes" : "No") << endl;
-
-  s = strstack.peek();
-  cout << "peek top of stack: " << s << endl;
-
-  cout << "pop top of stack" << endl;
-  strstack.pop();
-
-  s = strstack.peek();
-  cout << "peek top of stack: " << s << endl;
-
-  cout << "pop top of stack" << endl;
-  strstack.pop();
-
-  s = strstack.peek();
-  cout << "peek top of stack: " << s << endl;
-
-  cout << "pop top of stack" << endl;
-  strstack.pop();
-
-  cout << "Stack empty? " << ((strstack.isEmpty() == true) ? "Yes" : "No") << endl << endl;;
+  cout << s << " = " << c.CalculateFromInfix(s) << endl;
 
   return 0;
 }
